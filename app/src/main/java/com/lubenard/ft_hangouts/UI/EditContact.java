0a51b042f2b81d5996693f8b2cd0eb502d5d2eb6
lubenard <lubenard@student.42.fr>
2021-04-16 23:18:47 +0200

    Fixed Birthday not being editable + updated translations

--- app/src/main/java/com/lubenard/ft_hangouts/UI/EditContact.java
+++ app/src/main/java/com/lubenard/ft_hangouts/UI/EditContact.java
@@ -3,6 +3,7 @@ package com.lubenard.ft_hangouts.UI;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
@@ -23,6 +24,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
